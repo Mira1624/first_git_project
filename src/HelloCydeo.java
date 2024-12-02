de20cd4a916688783299441187e65bd8ab09c530
miraa <mira.kan100@gmail.com>
2024-12-01 20:28:48 -0800

    HelloCydeo updated again

--- src/HelloCydeo.java
+++ src/HelloCydeo.java
@@ -1,5 +1,5 @@
 public class HelloCydeo {
     public static void main(String[] args) {
-        System.out.println(" I am taking class at Cydeo");
+        System.out.println(" I am taking class at Cydeo since September");
     }
 }
