9032ee15ba7e0eda8a78392cf6d410473bf3d08a
Mira1624 <mira.kan100@gmail.com>
2024-12-16 22:41:19 -0800

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,7 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("I love coding");
+        System.out.println("I want to find job as QA around on March in 2025");
     }
 }
