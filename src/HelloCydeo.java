91402c271650c6b5200e0c07a263f35f7526bf12
Mira1624 <mira.kan100@gmail.com>
2024-12-16 22:29:28 -0800

    Update HelloCydeo.java

--- src/HelloCydeo.java
+++ src/HelloCydeo.java
@@ -1,5 +1,7 @@
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println(" I am taking class at Cydeo since September");
+        System.out.println(" my group in Cydeo is #11");
+        System.out.println(" my batches B38");
     }
 }
